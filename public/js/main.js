0e93b2134e9c754df6ef484a6861a5c74d882a7e
harrycjy1 <42664575+harrycjy1@users.noreply.github.com>
2018-12-07 19:18:14 +0900

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 (function() {
-	// Grab all HTML elements into variables
+	
 	var socket = io.connect();
 	var $messageForm = $('#message-form');
 	var $message = $('#message');
@@ -9,13 +9,13 @@
 	var $users = $('#users');
 	var $onlineUsersHeader = $('#online-users-header');
 	var $username = $('#username');
-	// Form submit to send messages
+	
 	$messageForm.submit(function(e) {
 		e.preventDefault();
 		socket.emit('send message', $message.val());
 		$message.val('');
 	});
-	// When a new message is sent, print username and time to interface
+	
 	socket.on('new message', function(data) {
 		var currentHours = new Date().getHours() > 9 ? new Date().getHours() : ('0' + new Date().getHours())
 		var currentMinutes = new Date().getMinutes() > 9 ? new Date().getMinutes() : ('0' + new Date().getMinutes())
@@ -23,7 +23,7 @@
 			$chat.append(`<li>[${currentHours}:${currentMinutes}]<strong> ${data.user}: </strong>${data.msg}</li>`) )
 			: alert('Blank message not allow!');
 	});
-	// Form submit to username
+	
 	$userForm.submit(function(e) {
 		e.preventDefault();
 		socket.emit('new user', $username.val(), function(data) {
@@ -34,7 +34,7 @@
 		});
 		$username.val('');
 	});
-	// get all users connected on localhost:3000 and print a list
+	
 	socket.on('get userList', function(data) {
 		var html = '';
 		for (i = 0; i < data.length; i++) {
