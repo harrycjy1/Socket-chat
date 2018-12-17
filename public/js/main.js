c4c84d810f0831cf58f332a6f998f9f1c959f9b4
harrycjy1 <42664575+harrycjy1@users.noreply.github.com>
2018-12-17 16:05:48 +0900

    hmm

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 (function() {
-	// Grab all HTML elements into variables
+	// 모든 html요소를 변수에 넣기
 	var socket = io.connect();
 	var $messageForm = $('#message-form');
 	var $message = $('#message');
@@ -9,13 +9,13 @@
 	var $users = $('#users');
 	var $onlineUsersHeader = $('#online-users-header');
 	var $username = $('#username');
-	// Form submit to send messages
+	// 폼 제출 후 메시지 전송
 	$messageForm.submit(function(e) {
 		e.preventDefault();
 		socket.emit('send message', $message.val());
 		$message.val('');
 	});
-	// When a new message is sent, print username and time to interface
+	//새 메시지 보낼때 사용자 이름과 시간 인터페이스에 출력
 	socket.on('new message', function(data) {
 		var currentHours = new Date().getHours() > 9 ? new Date().getHours() : ('0' + new Date().getHours())
 		var currentMinutes = new Date().getMinutes() > 9 ? new Date().getMinutes() : ('0' + new Date().getMinutes())
@@ -23,7 +23,7 @@
 			$chat.append(`<li>[${currentHours}:${currentMinutes}]<strong> ${data.user}: </strong>${data.msg}</li>`) )
 			: alert('Blank message not allow!');
 	});
-	// Form submit to username
+	//사용자 이름을 폼으로 제출
 	$userForm.submit(function(e) {
 		e.preventDefault();
 		socket.emit('new user', $username.val(), function(data) {
@@ -34,7 +34,7 @@
 		});
 		$username.val('');
 	});
-	// get all users connected on localhost:3000 and print a list
+	// 로컬에 접속한 유저 전부 가져와 목록 출력
 	socket.on('get userList', function(data) {
 		var html = '';
 		for (i = 0; i < data.length; i++) {
